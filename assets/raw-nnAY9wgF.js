de7712d3cdeaf13e3ac58a20e11188364e286978
Jeremy Muhlich <jmuhlich@bitflood.org>
2026-01-18 00:17:42 -0500

    update

---
+++ assets/raw-nnAY9wgF.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-Ba1GY9I3.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
